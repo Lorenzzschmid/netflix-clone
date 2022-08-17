6eac918db482db471d572ced2be5800c2a429acf
Lorenzzschmid <lorenz-schmid@gmx.net>
2022-08-17 16:28:04 +0200

    useEffect added to the Row component and the fetchUrl-Prop added to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Row from './components/Row';
+import requests from "../src/request"; 
 
 function App() {
   return (
     <div className="App">
      <h1>Hi, we'll try to do it today</h1>
-     <Row title="NETFLIX ORIGINALS"/>
-     <Row title="Trending Now" />
+     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+     
     </div>
   );
 }
